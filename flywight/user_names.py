8ce7cc2387cc91012d3e01ebae4ce78a732a19a5
Christian Rua <christian.rua@gmail.com>
2021-10-12 10:43:13 -0500

    adding flywight lesson

--- flywight/user_names.py
+++ flywight/user_names.py
@@ -1,5 +1,14 @@
+import string
+import random
+
 class User:
     def __init__(self, name) -> None:
         self.name = name
 
-        
+def random_string():
+    chars = string.ascii_lowercase
+    return ''.join(
+        [random.choise(chars) for in range(8)]
+    )
+
+if __name__ == '__main__':
